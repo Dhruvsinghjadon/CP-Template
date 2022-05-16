f3cd1b37bf9e418b7c575958340e186ab308f532
Dhruv Singh Jadon <dhruvsinghjadon102@gmail.com>
2022-05-16 19:17:08 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <climits>
+#include <bitset>
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+using namespace std;
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+ freopen("input.txt", "r" , stdin);
+ freopen("output.txt", "w" , stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+ 
+   cin>>t;
+
+    while (t--) {
+        
+
+}
+return 0;
+
+}
+
+
+  
+    
