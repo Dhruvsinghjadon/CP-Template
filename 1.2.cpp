8431e0a5648cef9eff49c27fa432b1a0e47e751a
Dhruv Singh Jadon <dhruvsinghjadon102@gmail.com>
2022-05-19 00:14:29 +0530

    Create 1.2.cpp

---
+++ 1.2.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+ 
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define Forn(i, n) for (int i = 1; i <= n; i++)
+#define loop(i, a, b) for (auto i = a; i <= b; i++)
+ 
+#define vct vector
+#define pb push_back
+ 
+#define ff first
+#define ss second
+ 
+#define sp " "
+#define br endl
+#define ret return
+ 
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define yess cout << "YES" << endl; return;
+#define noo cout << "NO" << endl; return;
+ 
+#define pr(a) cout << a
+#define prl(a) cout << a << endl
+#define prr(a) cout << a << endl; return
+#define prs(a) cout << a << " "
+#define prv(a) for (auto i : a) cout << i << " "; cout << endl;
+ 
+#define inti(a) int a; cin >> a
+#define int2(a, b) int a, b; cin >> a >> b
+#define lli(a) long long a; cin >> a;
+#define lli2(a, b) long long a, b; cin >> a >> b;
+#define stri(a) string a; cin >> a
+#define vcti(v, n) vector<int> v(n); for(int i = 0; i < n; i++) cin >> v[i];
+ 
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+ 
+#define len(a) a.length()
+#define sz(a) a.size()
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef unsigned int ui;
+typedef pair<int, int> pii;
+ 
+const ll MOD = 1e9+7;
+ 
+void solve() {
+    
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+freopen("Input.txt", "r", stdin);
+freopen("output.txt", "w" , stdout);
+#endif // ONLINE_JUDGE
+ 
+    int t;
+    t = 1;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+}
